ef3612d963f781356c3d1990de4482a0a0d61b50
StevenChou499 <stevenchou499@gmail.com>
2025-10-15 23:02:56 +0800

    Remove debug folder

--- Example_Filter/Debug/Core/Src/lpf.d
+++
@@ -1,2 +0,0 @@
-Core/Src/lpf.o: ../Core/Src/lpf.c ../Core/Inc/lpf.h
-../Core/Inc/lpf.h:
--- Example_Filter/Debug/SFRA/sfra.d
+++
@@ -1,3 +0,0 @@
-SFRA/sfra.o: D:/STM32Projects/SFRA_Implementation/SFRA/SFRA/sfra.c \
- D:/STM32Projects/SFRA_Implementation/SFRA/SFRA/sfra.h
-D:/STM32Projects/SFRA_Implementation/SFRA/SFRA/sfra.h:
